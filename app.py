260dea9c144a6e6bd05e6ebbf47f96ec237f349b
wasiqzahoor <wasiqzohoor1234@gmail.com>
2026-04-25 15:32:02 +0500

    add another file

--- app.py
+++ app.py
@@ -637,4 +637,5 @@ if __name__ == "__main__":
     print("=" * 60)
     print("  Open: http://localhost:5000")
     print("=" * 60 + "\n")
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
